96945a7859986a1d9ccca5f1725b083b78cdbdfe
Vadym Frolov <vdfroloff@gmail.com>
2022-07-07 21:27:53 +0300

    final

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './css/styles.css';
 import API from './js/fetchCountries';
 import countryList from '../src/templates/countryList.hbs';
 import countryInfo from '../src/templates/countryInfo.hbs';
+import { onFetchInfo, onFetchSuccess, onFetchError } from './js/notifications';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -11,18 +13,48 @@ const refs = {
   countryInfo: document.querySelector('.country-info'),
 };
 
-fetch('https://restcountries.com/v3.1/name/ukraine')
-  .then(response => {
-    if (!response.ok) {
-      onFetchError();
-      //! return Promise.reject(response.status);
-      //! throw new Error(response.status); - посмтреть что работает и лучше
-    }
-    return response.json();
-  })
-  .then(country => {
-    const countrymarkup = countryList(country);
-    // console.log('countrymarkup', countrymarkup);
-    const info = document.querySelector('.country-info');
-    info.innerHTML = countrymarkup;
-  });
+const onFormSearch = e => {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+
+  const searchQuery = refs.input.value.trim();
+
+  if (!searchQuery) {
+    return;
+  }
+
+  API.fetchCountries(searchQuery)
+    .then(getCountry)
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+const getCountry = country => {
+  if (country.length === 1) {
+    renderCountryCard(country[0]);
+    onFetchSuccess();
+    return;
+  } else if (country.length >= 2 && country.length <= 10) {
+    renderCountryList(country);
+    return;
+  } else if (country.length > 10) {
+    onFetchInfo();
+    return;
+  } else if (country.status === 404) {
+    onFetchError();
+    return;
+  }
+};
+
+const renderCountryCard = country => {
+  const markup = countryInfo(country);
+  refs.countryInfo.insertAdjacentHTML('beforeend', markup);
+};
+
+const renderCountryList = country => {
+  const markup = countryList(country);
+  refs.countryList.insertAdjacentHTML('beforeend', markup);
+};
+
+refs.input.addEventListener('input', debounce(onFormSearch, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,15 +2,25 @@ import { onFetchError } from './notifications';
 
 const BASE_URL = 'https://restcountries.com/v3.1/';
 
+// const search = new URLSearchParams({
+//   name,
+//   capital,
+//   population,
+//   flags,
+//   languages,
+// });
+
 const fetchCountries = name => {
-  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`).then(
+  return fetch(`${BASE_URL}name/${name}?fields=name,capital,population,flags,languages`).then(
+    // return fetch(`${BASE_URL}name/${name}?fields=${search}`).then(
     response => {
       if (!response.ok) {
         onFetchError();
-        //! return Promise.reject(response.status);
-        //! throw new Error(response.status); - посмтреть что работает и лучше
+        throw new Error(response.status);
       }
       return response.json();
     }
   );
 };
+
+export default { fetchCountries };
